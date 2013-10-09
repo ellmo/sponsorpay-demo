a21d915a0a8b0878e8e98a68bb82c57d4d3bbfca
ellmo <ellmunzai@gmail.com>
2013-10-09 14:23:11 +0200

    INIT

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'faraday'
+gem 'haml-rails'
+gem 'haml_coffee_assets'
+gem 'jquery-rails'
+gem 'jquery-rails'
+gem 'rabl'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'sass-rails', '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'pry-rails'
+  gem 'rspec-rails'
+end
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'sextant'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sponsorpay::Application.config.secret_token = '88e2ffa78e0a5b91f80477c6acc8a3a0463fbe39e10e2ba74e5559390ca8bab8eb5b29e5cf5c94dd066f7b23eb4e90cdf919185d7611f25ac54afba570519521'
